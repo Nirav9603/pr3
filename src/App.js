568288de7f5cf1843d202565f5f1d3a1db22e16c
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-13 10:03:55 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Component/Header';
-import Footer from './Component/Footer';
-import Home from './Container/Home';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import Header from './Component/Header/Header';
+import Footer from './Component/Footer/Footer';
+import Home from './Container/Home/Home';
+import About from './Container/About/About';
+import Departments from './Container/Departments/Departments';
+import Doctors from './Container/Doctors/Doctors';
+import Contact from './Container/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+        <Switch>
+          <Route path={"/"} exact component={Home} />
+          <Route path={"/departments"} exact component={Departments} />
+          <Route path={"/doctors"} exact component={Doctors} />
+          <Route path={"/about"} exact component={About} />
+          <Route path={"/contact"} exact component={Contact} />
+        </Switch>
       <Footer />
     </div>
   );
